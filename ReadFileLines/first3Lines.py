7f39d3d10a0f08b2d0b65034aff95fb967e9a96e
HeyNaNd0 <22282823+HeyNaNd0@users.noreply.github.com>
2025-10-19 08:48:29 -0500

    first 3 lines of a file completed

--- ReadFileLines/first3Lines.py
+++ ReadFileLines/first3Lines.py
@@ -4,4 +4,8 @@ creating this to learn how to read input from a random file
 
 with open('randomLines.txt', 'r') as file:
     content = file.read()
-    print(type(content))
+    lines=content.split('.')
+    first3Lines=lines[0:3]
+    print("First 3 lines of the file:")
+    for line in first3Lines:
+        print(f"{line.strip()}.")
